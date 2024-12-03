25e012a800094ec1fbe40d726ff6dc653844c3c0
matheusclima <matheuslima1607@gmail.com>
2024-12-03 20:45:32 -0300

    b

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,9 +5,8 @@ const api = axios.create({
     baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type',
-    }
+    },
+    withCredentials: true,
 });
 
 api.defaults.withCredentials = true;
