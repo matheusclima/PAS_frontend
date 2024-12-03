ecdc8c790f4c4be7ff9009c5552548027be88c5e
matheusclima <matheuslima1607@gmail.com>
2024-12-03 20:40:05 -0300

    a

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pas_backend.onrender.com';
+const BASE_URL = 'https://pas-backend-zhsi.onrender.com/';
 const api = axios.create({
     baseURL: BASE_URL,
     headers: {
